ffed5287278bea7e1069df65b676da1337e08ba7
wadhahjemai <wadhah.jemai96@gmail.com>
2024-11-13 16:14:53 +0100

    feat(provider): implement Metadata and Schema definition

---
+++ examples/provider-install-verification/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    tools={
+      source="registry.terraform.io/Wa4h1h/tools"
+    }
+  }
+}
+
+provider "tools" {}
--- main.go
+++ main.go
@@ -8,35 +8,35 @@ import (
 	"flag"
 	"log"
 
+	"github.com/Wa4h1h/terraform-provider-tools/internal/provider"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
 )
 
-var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary.
-	version string = "dev"
+// these will be set by the goreleaser configuration
+// to appropriate values for the compiled binary.
+var version string = "dev"
 
-	// goreleaser can pass other information to the main package, such as the specific commit
-	// https://goreleaser.com/cookbooks/using-main.version/
-)
+// goreleaser can pass other information to the main package, such as the specific commit
+// https://goreleaser.com/cookbooks/using-main.version/
 
 func main() {
 	var debug bool
 
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.BoolVar(&debug, "debug",
+		false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
 		// TODO: Update this string with the published name of your provider.
 		// Also update the tfplugindocs generate command to either remove the
 		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Address: "registry.terraform.io/Wa4h1h/tools",
 		Debug:   debug,
 	}
 
-	err := providerserver.Serve(context.Background(), provider.New(version), opts)
-
+	err := providerserver.Serve(context.Background(),
+		provider.NewToolsProvider(version), opts)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
